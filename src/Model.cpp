26e902f004c53892643d95601a30f68aecd6ba82
CarolinS <carolingit@t-online.de>
2020-04-14 16:02:09 +0200

    draft for Model

---
+++ src/Model.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file   Model.cpp
+ * @author Carolin
+ * @date   14.04.2020 (creation)
+ * @brief  Definition of the model class (contains all Info needed by Clients).
+ */
+
+#include "Model.hpp"
+
---
+++ src/Model.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file   Model.hpp
+ * @author Carolin
+ * @date   14.04.2020 (creation)
+ * @brief  Declaration of the model class (contains all Info needed by Clients).
+ */
+
+#ifndef SAMPLELIBRARY_LIBRARY_H
+#define SAMPLELIBRARY_LIBRARY_H
+
+#include <string>
+#include <iostream>
+
+
+class Model {
+public:
+
+    Model() = default;
+
+    //TODO set up model components (with LibCommon)
+
+};
+
+#endif //SAMPLELIBRARY_LIBRARY_H
