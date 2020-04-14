f71dc32f98b1129b19c22d898ab3d05a05281b2e
CarolinS <carolingit@t-online.de>
2020-04-14 15:58:10 +0200

    new dependency and submodule

--- installDependencies.sh
+++ installDependencies.sh
@@ -5,7 +5,25 @@
 set -e
 
 
-# dependencies here
+# WebsocketCPP
+# Libwebsockets
+sudo apt install libssl-dev
+cd /tmp
+git clone https://github.com/warmcat/libwebsockets.git
+cd libwebsockets
+mkdir build && cd build
+cmake -DJSON_BuildTests=false ..
+make -j$(nproc)
+sudo make install
+sudo ldconfig
+# WebsocketCPP
+cd /tmp
+git clone https://github.com/SoPra-Team-17/WebsocketCPP.git
+cd WebsocktesCPP
+mkdir build && cd build
+cmake -DJSON_BuildTests=false ..
+make -j$(nproc)
+sudo make install
 
 
 sudo ldconfig
